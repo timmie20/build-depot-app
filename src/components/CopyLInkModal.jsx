f7bfb1778c4ce63a16da8e3c90ac7917146ab3ce
Jeffrey Uzoamaka <ikechukwujeffrey19@gmail.com>
2023-12-31 19:56:56 +0100

    Fixed copy modal overflow

--- src/components/CopyLInkModal.jsx
+++ src/components/CopyLInkModal.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const CopyLInkModal = ({ setOpenLinkModal, url }) => {
   const [isCopied, setIsCopied] = useState(false)
+  const [displayWidth, setDisplayWidth] = useState(window.innerWidth)
     
   const copyToClipboard = async (text) => {
     if ('clipboard' in navigator) {
@@ -28,7 +29,8 @@ const CopyLInkModal = ({ setOpenLinkModal, url }) => {
     <div className="w-full bg-slate-700/70 p-3 fixed inset-0 z-50 grid place-items-center overflow-y-scroll cursor-pointer">
       <div className="flex flex-col gap-4 bg-white px-3 py-6 rounded-lg w-full max-w-lg shadow-xl cursor-default relative overflow-hidden md:gap-7 md:px-5 md:py-10">
         <div className="flex items-center justify-between bg-slate-100 p-2 rounded-md">
-            <p className="text-[15px] text-slate-500">{url.length < 46 ? url : url.slice(0, 46) + '...'}</p>
+            {displayWidth > 476 ? <p className="text-[15px] text-slate-500">{url.length < 46 ? url : url.slice(0, 46) + '...'}</p> :
+            <p className="text-[15px] text-slate-500">{url.length < 46 ? url : url.slice(0, 15) + '...'}</p>}
             <button onClick={handleCopyClick} className={`w-fit flex items-center justify-center py-2 px-3 ${isCopied ? 'bg-[#48C546]' : 'bg-orange-clr-full'} rounded-md text-white`}>
                 {isCopied ? 'Copied!' : 'Copy Link'}
             </button>
